84c9d7af176c99b774377adc0824af7f81566854
Hunter, Kirsten <khunter@akamai.com>
2017-08-07 18:37:18 -0700

    Fixing static typo

--- Chapter2/02_04/Start/express-server.js
+++ Chapter2/02_04/Start/express-server.js
@@ -16,7 +16,7 @@ var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser');
 
-app.use('/demo', express.static(path.join(__dirname, '..', '..', static')));
+app.use('/demo', express.static(path.join(__dirname, '..', '..', 'static')));
 app.use(bodyParser.json());
 app.set('json spaces', 2);
 
